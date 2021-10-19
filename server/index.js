5f661a9861ed9d7edfdd94cf6b639604b09e86d5
nusratjahan1535 <nusratjahan1535@gmail.com>
2021-10-19 18:16:13 +0600

    Title image added

--- server/index.js
+++ server/index.js
@@ -3,20 +3,22 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import postRoutes from './routes/posts.js'
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-// app.get('/',(req,res)=>{res.send("hello I am easyShop")});
+app.get('/',(req,res)=>{res.send("hello to memories API")});
 // app.listen(5001);
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts',postRoutes); //it's means every routes of postRoutes will start with 'http://localhost:5000/posts/' instead of 'http://localhost:5000/'
 
-const CONNECTION_URL = 'mongodb+srv://bigbossadmin:bigbossadmin123@cluster0.njvrz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
